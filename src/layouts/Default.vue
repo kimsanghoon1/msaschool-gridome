dcc84ebaa2c7a77c61864e0ece171e5dd4ad84b4
kimsanghoon1 <sanghoon01@uengine.org>
2020-12-29 14:36:34 +0900

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <div>
+      <v-app-bar>
+        <v-btn icon :to="$url('/')" title="Home">
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{ $static.metadata.siteName }}</v-toolbar-title>
+        <v-spacer />
+
+        <v-btn :to="$url('/')">
+          Home
+        </v-btn>
+        <v-btn :to="$url('/about')">
+          About
+        </v-btn>
+      </v-app-bar>
+    </div>
+    <v-container>
+      <v-card>
+        <slot />
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<static-query>
+  query {
+    metadata {
+      siteName
+    }
+  }
+</static-query>
